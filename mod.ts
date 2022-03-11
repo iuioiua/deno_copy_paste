830b0c6bbcad8348e707b7919f37f85aa1798311
Asher Gomez <ashersaupingomez@gmail.com>
2022-03-12 08:06:06 +1100

    Changed: try new method for windows

--- mod.ts
+++ mod.ts
@@ -12,7 +12,7 @@ export async function copy(text: string): Promise<void> {
   const cmd = {
     "darwin": ["pbcopy"],
     "linux": ["xclip", "-selection", "clipboard", "-i"],
-    "windows": ["powershell", "-Command", "Set-Clipboard"],
+    "windows": ["powershell", "-noprofile", "-command", "$input|Set-Clipboard"],
   }[Deno.build.os];
   const process = await Deno.run({ cmd, stdin: "piped" });
   await writeAll(process.stdin, new TextEncoder().encode(text));
@@ -33,7 +33,7 @@ export async function paste(): Promise<string> {
   const cmd = {
     "darwin": ["pbpaste"],
     "linux": ["xclip", "-selection", "clipboard", "-o"],
-    "windows": ["powershell", "-Command", "Get-Clipboard"],
+    "windows": ["powershell", "-noprofile", "-command", "Get-Clipboard"],
   }[Deno.build.os];
   const process = await Deno.run({ cmd, stdout: "piped" });
   const output = await process.output();
