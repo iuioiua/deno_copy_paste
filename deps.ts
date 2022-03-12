01593f8db1602e1de780c3efa6fa50e14154f0f4
Asher Gomez <ashersaupingomez@gmail.com>
2022-03-12 21:44:30 +1100

    Changes: test using assertStrictEquals

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export { writeAll } from "https://deno.land/std@0.129.0/streams/conversion.ts";
-export { assert } from "https://deno.land/std@0.129.0/testing/asserts.ts";
+export { assertStrictEquals } from "https://deno.land/std@0.129.0/testing/asserts.ts";
--- test.ts
+++ test.ts
@@ -1,11 +1,11 @@
 import { copy, paste } from "./mod.ts";
-import { assert } from "./deps.ts";
+import { assertStrictEquals } from "./deps.ts";
 
 Deno.test({
   name: "copy and paste",
   fn: async () => {
     const text = crypto.randomUUID();
     await copy(text);
-    assert(await paste() === text);
+    assertStrictEquals(text, await paste());
   },
 });
