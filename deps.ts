674ecce9d83fcf70f3ec9ddeb8cebab1a8438e3a
Asher Gomez <ashersaupingomez@gmail.com>
2022-04-19 14:42:39 +1000

    Updates: dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { writeAll } from "https://deno.land/std@0.134.0/streams/conversion.ts";
-export { assert } from "https://deno.land/std@0.134.0/testing/asserts.ts";
+export { writeAll } from "https://deno.land/std@0.135.0/streams/conversion.ts";
+export { assert } from "https://deno.land/std@0.135.0/testing/asserts.ts";
